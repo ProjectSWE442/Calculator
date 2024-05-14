4f67f8732451b4f4dea3bbc109fefdba7bc920dc
SolaimanSH <431101284@ju.edu.sa>
2024-05-14 15:31:53 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -48,14 +48,14 @@ public class Calculator {
     }
 
     public static void multiply(double num1, double num2) {
-        System.out.println(num1 + " + " + num2 " = " + (num1+num2));
+        System.out.println(num1 + " * " + num2 " = " + (num1*num2));
     }
 
     public static void divide(double num1, double num2) {
         
-        if(num1 == 0 || num2 == 0)
+        if( num2 == 0)
         System.out.println("Error: Cannot devide by 0");
         else
-        System.out.println(num1 + " * " + num2 " = " + (num1*num2));
+        System.out.println(num1 + " / " + num2 " = " + (num1/num2));
     }
 }
